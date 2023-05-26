5468aa858656bba2b93162dc9438cf9e056b7873
Ninjjaak <134675690+Ninjjaak@users.noreply.github.com>
2023-05-26 13:39:18 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import os 
+from apikey import apikey
+import streamlit as st 
+from langchain.llms import OpenAI
+
+os.environ['OPENAI_API_KEY'] = apikey 
+
+st.title('AutoGPT')
+prompt = st.text_input('your prompt here')
