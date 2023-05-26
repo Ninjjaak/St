51bf76756b6f3b8ec0194e7d3ba5761e0dc7bc00
Ninjjaak <134675690+Ninjjaak@users.noreply.github.com>
2023-05-26 13:40:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,2 @@
-import os 
-from apikey import apikey
 import streamlit as st 
-from langchain.llms import OpenAI
-
-os.environ['OPENAI_API_KEY'] = apikey 
-
-st.title('AutoGPT')
-prompt = st.text_input('your prompt here')
+st.write("hey there")
